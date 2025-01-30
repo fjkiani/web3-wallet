d73656064a94c34c4fd45ffd7fff7ba1e06804c4
Fahad Kiani <fjkiani@icloud.com>
2025-01-29 21:00:08 -0500

    changes for deployment

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from "./Transactions.json";
 
-export const contractAddress = "0x00ecea2ed8eb01917446A1D90117C69D87d9DBb0";
+export const contractAddress = import.meta.env.VITE_CONTRACT_ADDRESS;
 export const contractABI = abi.abi;
