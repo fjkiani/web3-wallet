b592e1af2860e2b2e0bf3a7f577d4a4d1b3ac05d
Fahad Kiani <fjkiani@icloud.com>
2025-01-29 21:04:11 -0500

    fixing failed link deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repo-name/',
+  // Remove the base if you're not using GitHub Pages
+  // base: '/your-repo-name/',
 })
